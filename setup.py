0c05d2d0feb9aeaadab57db2d372c26f43a89c26
Anthony Delosa <adelosa@gmail.com>
2019-09-22 12:16:32 +1000

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open('README.rst', 'r').read()
 
 setup(name='cardutil',
       packages=find_packages(),
-      version='0.1.6',
+      version='0.1.7',
       license='MIT',
       description='Python package for working with payment card systems',
       long_description=readme,
