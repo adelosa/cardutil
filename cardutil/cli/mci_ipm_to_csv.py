2f0a810a894903823e3922a4abd82bd43666fda4
Anthony Delosa <adelosa@gmail.com>
2020-05-28 21:07:46 +1000

    Fix ipm to csv handling of non-ascii chars. Requires specification of encoding params.

--- cardutil/cli/mci_ipm_to_csv.py
+++ cardutil/cli/mci_ipm_to_csv.py
@@ -7,12 +7,16 @@ from cardutil.outputter import dicts_to_csv
 
 
 def cli_entry():
-    args = vars(cli_parser().parse_args())
-    if not args['out_filename']:
-        args['out_filename'] = args['in_filename'] + '.csv'
+    cli_run(**vars(cli_parser().parse_args()))
 
-    with open(args['in_filename'], 'rb') as in_ipm, open(args['out_filename'], 'w') as out_csv:
-        mci_ipm_to_csv(in_ipm=in_ipm, out_csv=out_csv, **args)
+
+def cli_run(**kwargs):
+    if not kwargs.get('out_filename'):
+        kwargs['out_filename'] = kwargs['in_filename'] + '.csv'
+
+    with open(kwargs['in_filename'], 'rb') as in_ipm:
+        with open(kwargs['out_filename'], 'w', encoding=kwargs['out_encoding']) as out_csv:
+            mci_ipm_to_csv(in_ipm=in_ipm, out_csv=out_csv, **kwargs)
 
 
 def cli_parser():
@@ -20,6 +24,7 @@ def cli_parser():
     parser.add_argument('in_filename')
     parser.add_argument('-o', '--out-filename')
     parser.add_argument('--in-encoding', default='ascii')
+    parser.add_argument('--out-encoding', default='ascii')
     parser.add_argument('--no1014blocking', action='store_true')
     add_version(parser)
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
 import csv
 import io
+import os
+import tempfile
 import unittest
 
 from cardutil.mciipm import VbsWriter
@@ -94,11 +96,43 @@ class CliTestCase(unittest.TestCase):
         do_test(no_blocking=False)
         do_test(no_blocking=True)
 
+    def test_ipm_to_csv_bad_data_de38(self):
+        """
+        prod issue - auth code DE38 contains non-ascii characters.
+        Added new out-encoding parameter to support this -- changed to latin_1 which supports full 256 bits.
+        Also need to set in-encoding to latin_1 for the same reason
+        I have only seen this issue on one transaction once after processing 1000's of input files
+        """
+        in_ipm = io.BytesIO(b'\x00\x00\x00\x1a0100\x80\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+                            b'n\x9cm\x9cl\x9c\x00\x00\x00\x00')
+        with tempfile.TemporaryFile(mode='w', encoding='latin_1') as out_csv:
+            mci_ipm_to_csv.mci_ipm_to_csv(
+                in_ipm=in_ipm, out_csv=out_csv, no1014blocking=True, in_encoding='latin_1')
+
+    def test_ipm_to_csv_input_params(self):
+        """
+        Actually run using real files
+        :return:
+        """
+        in_ipm_data = (b'\x00\x00\x00\x1a0100\x80\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+                       b'nXmXlX\x00\x00\x00\x00')
+
+        with tempfile.NamedTemporaryFile(mode='w+b', delete=False) as in_ipm:
+            in_ipm.write(in_ipm_data)
+            in_ipm_name = in_ipm.name
+            print(in_ipm_name)
+            in_ipm.close()
+        mci_ipm_to_csv.cli_run(in_filename=in_ipm_name, out_encoding='ascii')
+        mci_ipm_to_csv.cli_run(in_filename=in_ipm_name, out_filename=in_ipm_name + '.csv', out_encoding='ascii')
+        os.remove(in_ipm_name)
+        os.remove(in_ipm_name + '.csv')
+
     def test_mci_ipm_to_csv_cli_parser(self):
         args = vars(mci_ipm_to_csv.cli_parser().parse_args(['file1.ipm']))
         self.assertEqual(
             args,
-            {'in_encoding': 'ascii', 'in_filename': 'file1.ipm', 'no1014blocking': False, 'out_filename': None})
+            {'in_encoding': 'ascii', 'out_encoding': 'ascii', 'in_filename': 'file1.ipm', 'no1014blocking': False,
+             'out_filename': None})
 
     def test_mci_csv_to_ipm_cli_parser(self):
         args = vars(mci_csv_to_ipm.cli_parser().parse_args(['file1.ipm']))
@@ -110,7 +144,7 @@ class CliTestCase(unittest.TestCase):
 def print_stream(stream, description):
     stream.seek(0)
     data = stream.read()
-    print(description)
+    print('***' + description + '***')
     print(data)
     stream.seek(0)
 
